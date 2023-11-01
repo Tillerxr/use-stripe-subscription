2f25e5f975d4fab2ca071090fb2b4a6b6ab4ea76
Andy Baird <andy.baird@icloud.com>
2023-11-01 10:24:50 -0600

    adding support for promotion codes

--- src/client.tsx
+++ src/client.tsx
@@ -1,181 +1,182 @@
-import React, { createContext, useContext, useMemo, ReactNode } from "react";
-import useSWR, { SWRConfig } from "swr";
-import { loadStripe, Stripe } from "@stripe/stripe-js";
+import React, { createContext, useContext, useMemo, ReactNode } from "react"
+import useSWR, { SWRConfig } from "swr"
+import { loadStripe, Stripe } from "@stripe/stripe-js"
 
 const StripeContext = createContext<{
-  clientPromise:Promise<Stripe | null>;
-  endpoint:string;
-}>(null!);
+	clientPromise: Promise<Stripe | null>
+	endpoint: string
+}>(null!)
 
 export const SubscriptionProvider = ({
-  children,
-  endpoint,
-  stripePublishableKey,
+	children,
+	endpoint,
+	stripePublishableKey,
 }: {
-  stripePublishableKey: string;
-  children: ReactNode;
-  endpoint?: string;
+	stripePublishableKey: string
+	children: ReactNode
+	endpoint?: string
 }) => {
-  const stripeClient = useMemo(() => loadStripe(stripePublishableKey), [
-    stripePublishableKey,
-    loadStripe,
-  ]);
-  endpoint = endpoint || "/api/subscription";
-  return (
-    <StripeContext.Provider
-      value={{ clientPromise: stripeClient, endpoint: endpoint }}
-    >
-      <SWRConfig
-        value={{
-          fetcher: async (args) => {
-            const data = await fetch(args);
-            return await data.json();
-          },
-        }}
-      >
-        {children}
-      </SWRConfig>
-    </StripeContext.Provider>
-  );
-};
+	const stripeClient = useMemo(
+		() => loadStripe(stripePublishableKey),
+		[stripePublishableKey, loadStripe]
+	)
+	endpoint = endpoint || "/api/subscription"
+	return (
+		<StripeContext.Provider
+			value={{ clientPromise: stripeClient, endpoint: endpoint }}
+		>
+			<SWRConfig
+				value={{
+					fetcher: async (args) => {
+						const data = await fetch(args)
+						return await data.json()
+					},
+				}}
+			>
+				{children}
+			</SWRConfig>
+		</StripeContext.Provider>
+	)
+}
 
 export interface redirectToCheckoutArgs {
-  price: string;
-  successUrl?: string;
-  cancelUrl?: string;
+	price: string
+	successUrl?: string
+	cancelUrl?: string
+	allowPromotionCodes?: boolean
 }
 
 export interface redirectToCustomerPortalArgs {
-  returnUrl?: string;
+	returnUrl?: string
 }
 
 export function useSubscription() {
-  const { endpoint } = useContext<{
-    clientPromise: Promise<Stripe | null>;
-    endpoint:string}
-  >(StripeContext);
-  const { data, error } = useSWR(`${endpoint}?action=useSubscription`);
+	const { endpoint } = useContext<{
+		clientPromise: Promise<Stripe | null>
+		endpoint: string
+	}>(StripeContext)
+	const { data, error } = useSWR(`${endpoint}?action=useSubscription`)
 
-  // Also wait for customer to load
-  if (!data) {
-    return {
-      isLoaded: false,
-    } as {
-      isLoaded: false;
-      subscription: undefined;
-      products: undefined;
-      redirectToCheckout: undefined;
-      redirectToCustomerPortal: undefined;
-    };
-  }
+	// Also wait for customer to load
+	if (!data) {
+		return {
+			isLoaded: false,
+		} as {
+			isLoaded: false
+			subscription: undefined
+			products: undefined
+			redirectToCheckout: undefined
+			redirectToCustomerPortal: undefined
+		}
+	}
 
-  const { products, subscription } = data;
+	const { products, subscription } = data
 
-  const redirectToCheckout = async (args: redirectToCheckoutArgs) => {
-    if (!args.successUrl) {
-      args.successUrl = window.location.href;
-    }
-    if (!args.cancelUrl) {
-      args.cancelUrl = window.location.href;
-    }
-    const sessionResponse = await fetch(
-      `${endpoint}?action=redirectToCheckout`,
-      {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(args),
-      }
-    );
-    const session = await sessionResponse.json();
-    window.location.href = session.url;
-  };
+	const redirectToCheckout = async (args: redirectToCheckoutArgs) => {
+		if (!args.successUrl) {
+			args.successUrl = window.location.href
+		}
+		if (!args.cancelUrl) {
+			args.cancelUrl = window.location.href
+		}
+		const sessionResponse = await fetch(
+			`${endpoint}?action=redirectToCheckout`,
+			{
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(args),
+			}
+		)
+		const session = await sessionResponse.json()
+		window.location.href = session.url
+	}
 
-  const redirectToCustomerPortal = async (
-    args: redirectToCustomerPortalArgs
-  ) => {
-    args = args || {};
-    if (!args.returnUrl) {
-      args.returnUrl = window.location.href;
-    }
-    const sessionResponse = await fetch(
-      `${endpoint}?action=redirectToCustomerPortal`,
-      {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(args),
-      }
-    );
-    const session = await sessionResponse.json();
-    window.location.href = session.url;
-  };
+	const redirectToCustomerPortal = async (
+		args: redirectToCustomerPortalArgs
+	) => {
+		args = args || {}
+		if (!args.returnUrl) {
+			args.returnUrl = window.location.href
+		}
+		const sessionResponse = await fetch(
+			`${endpoint}?action=redirectToCustomerPortal`,
+			{
+				method: "post",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(args),
+			}
+		)
+		const session = await sessionResponse.json()
+		window.location.href = session.url
+	}
 
-  return {
-    isLoaded: true,
-    products,
-    subscription,
-    redirectToCheckout,
-    redirectToCustomerPortal,
-  };
+	return {
+		isLoaded: true,
+		products,
+		subscription,
+		redirectToCheckout,
+		redirectToCustomerPortal,
+	}
 }
 
 interface GateProps {
-  product?: any;
-  unsubscribed?: boolean;
-  feature?: string;
-  negate?: boolean;
-  children?: ReactNode;
+	product?: any
+	unsubscribed?: boolean
+	feature?: string
+	negate?: boolean
+	children?: ReactNode
 }
 export const Gate = ({
-  product,
-  negate,
-  feature,
-  unsubscribed,
-  children,
+	product,
+	negate,
+	feature,
+	unsubscribed,
+	children,
 }: GateProps) => {
-  const { isLoaded, products, subscription } = useSubscription();
+	const { isLoaded, products, subscription } = useSubscription()
 
-  if ([!!unsubscribed, !!product, !!feature].filter((x) => x).length !== 1) {
-    throw new Error(
-      `Please pass exactly one of unsubscribed, product, or feature to Gate`
-    );
-  }
+	if ([!!unsubscribed, !!product, !!feature].filter((x) => x).length !== 1) {
+		throw new Error(
+			`Please pass exactly one of unsubscribed, product, or feature to Gate`
+		)
+	}
 
-  if (!isLoaded) {
-    return null;
-  }
+	if (!isLoaded) {
+		return null
+	}
 
-  let condition;
-  if (unsubscribed) {
-    condition = subscription === null;
-  }
+	let condition
+	if (unsubscribed) {
+		condition = subscription === null
+	}
 
-  if (product || feature) {
-    if (subscription === null) {
-      return null;
-    }
-    condition = false;
-    for (let item of subscription.items.data) {
-      if (product && item.price.product === product.id) {
-        condition = true;
-      } else if (feature) {
-        const productFeatures =
-          products
-            .find((x:any) => x.product.id === item.price.product)
-            .product.metadata.features?.split(",") || [];
-        for (let productFeature of productFeatures) {
-          if (productFeature === feature) {
-            condition = true;
-          }
-        }
-      }
-    }
-  }
+	if (product || feature) {
+		if (subscription === null) {
+			return null
+		}
+		condition = false
+		for (let item of subscription.items.data) {
+			if (product && item.price.product === product.id) {
+				condition = true
+			} else if (feature) {
+				const productFeatures =
+					products
+						.find((x: any) => x.product.id === item.price.product)
+						.product.metadata.features?.split(",") || []
+				for (let productFeature of productFeatures) {
+					if (productFeature === feature) {
+						condition = true
+					}
+				}
+			}
+		}
+	}
 
-  return (!negate && condition) || (negate && !condition) ? (
-    <>{children}</>
-  ) : null;
-};
+	return (!negate && condition) || (negate && !condition) ? (
+		<>{children}</>
+	) : null
+}
--- src/server.ts
+++ src/server.ts
@@ -1,160 +1,174 @@
-import Stripe from "stripe";
+import Stripe from "stripe"
 
-export interface Subscription extends Stripe.Subscription{}
+export interface Subscription extends Stripe.Subscription {}
 
 export const stripeApiClient = new Stripe(`${process.env.STRIPE_SECRET_KEY}`, {
-  apiVersion:'2020-08-27'
-});
+	apiVersion: "2020-08-27",
+})
 
 export interface CustomerHasFeatureArgs {
-  customerId: string;
-  feature: string;
+	customerId: string
+	feature: string
 }
-export const customerHasFeature = async ({ customerId, feature }:CustomerHasFeatureArgs) => {
-  const customer = (await stripeApiClient.customers.retrieve(customerId, {
-    expand: ["subscriptions"],
-  })) as Stripe.Customer;
-  let subscription:Stripe.Subscription | null =  customer.subscriptions ? customer.subscriptions.data[0] || null : null;
-  if (subscription) {
-    subscription = await stripeApiClient.subscriptions.retrieve(
-      subscription.id,
-      { expand: ["items.data.price.product"] }
-    );
-    const features = (subscription.items.data[0].price
-      .product as Stripe.Product).metadata.features;
-    return features?.includes(feature);
-  }
-  return false;
-};
+export const customerHasFeature = async ({
+	customerId,
+	feature,
+}: CustomerHasFeatureArgs) => {
+	const customer = (await stripeApiClient.customers.retrieve(customerId, {
+		expand: ["subscriptions"],
+	})) as Stripe.Customer
+	let subscription: Stripe.Subscription | null = customer.subscriptions
+		? customer.subscriptions.data[0] || null
+		: null
+	if (subscription) {
+		subscription = await stripeApiClient.subscriptions.retrieve(
+			subscription.id,
+			{ expand: ["items.data.price.product"] }
+		)
+		const features = (
+			subscription.items.data[0].price.product as Stripe.Product
+		).metadata.features
+		return features?.includes(feature)
+	}
+	return false
+}
 
 export interface SubscriptionHandlerArgs {
-  customerId: string;
-  query: {
-    action:string;
-  };
-  body:any;
+	customerId: string
+	query: {
+		action: string
+	}
+	body: any
 }
-export const subscriptionHandler = async ({ customerId, query, body }:SubscriptionHandlerArgs) => {
-  if (query.action === "useSubscription") {
-    return await useSubscription({ customerId });
-  }
+export const subscriptionHandler = async ({
+	customerId,
+	query,
+	body,
+}: SubscriptionHandlerArgs) => {
+	if (query.action === "useSubscription") {
+		return await useSubscription({ customerId })
+	}
 
-  if (query.action === "redirectToCheckout") {
-    return await redirectToCheckout({ customerId, body });
-  }
+	if (query.action === "redirectToCheckout") {
+		return await redirectToCheckout({ customerId, body })
+	}
 
-  if (query.action === "redirectToCustomerPortal") {
-    return await redirectToCustomerPortal({ customerId, body });
-  }
+	if (query.action === "redirectToCustomerPortal") {
+		return await redirectToCustomerPortal({ customerId, body })
+	}
 
-  return { error: "Action not found" };
-};
-
-export interface UseSubscriptionArgs{
-  customerId:string;
-}
-async function useSubscription({ customerId }:UseSubscriptionArgs) {
-  // Retrieve products based on default billing portal config
-
-  // First, retrieve the configuration
-  const configurations = await stripeApiClient.billingPortal.configurations.list(
-    {
-      is_default: true,
-      expand: ["data.features.subscription_update.products"],
-    }
-  );
-  if (!configurations)
-    return null;
-
-  // Stripe doesn't let us expand as much as we'd like.
-  // Run this big mess to manually expand
-
-//  configurations.data[0].features.subscription_update.products.length;
-
-  // We preserve the order stripe returns things in
-  const products = new Array(
-    configurations.data[0].features.subscription_update.products?.length
-  );
-  const pricePromises = (configurations.data[0].features.subscription_update.products || [])
-    .map((product, i) =>
-      product.prices.map(async (price, j) => {
-        const priceData = await stripeApiClient.prices.retrieve(price, {
-          expand: ["product"],
-        });
-        const cleanPriceData = {
-          ...priceData,
-          product: (priceData.product as Stripe.Product).id,
-        };
-        if (!products[i]) {
-          products[i] = {
-            product: priceData.product,
-            prices: new Array(product.prices.length),
-          };
-          products[i].prices[j] = cleanPriceData;
-        } else {
-          products[i].prices[j] = cleanPriceData;
-        }
-      })
-    )
-    .flat();
-
-  let subscription:Stripe.Subscription | null = null;
-  const subscriptionPromise = new Promise<void>(async (resolve) => {
-    const customer:any = await stripeApiClient.customers.retrieve(customerId, { expand: ["subscriptions"] })
-    subscription = customer.subscriptions.data[0] || null;
-    if (subscription)
-    {
-      subscription = await stripeApiClient.subscriptions.retrieve(subscription.id, {
-        expand: ["plan.product"],
-      });
-    }
-    resolve()
-  })
-
-  await Promise.all([...pricePromises, subscriptionPromise]);
-
-  return { products, subscription };
+	return { error: "Action not found" }
 }
 
-export interface RedirectArgs{
-  customerId:string;
-  body:any;
+export interface UseSubscriptionArgs {
+	customerId: string
 }
-async function redirectToCustomerPortal({ customerId, body }:RedirectArgs) {
-  return await stripeApiClient.billingPortal.sessions.create({
-    customer: customerId,
-    return_url: body.returnUrl,
-  });
+async function useSubscription({ customerId }: UseSubscriptionArgs) {
+	// Retrieve products based on default billing portal config
+
+	// First, retrieve the configuration
+	const configurations =
+		await stripeApiClient.billingPortal.configurations.list({
+			is_default: true,
+			expand: ["data.features.subscription_update.products"],
+		})
+	if (!configurations) return null
+
+	// Stripe doesn't let us expand as much as we'd like.
+	// Run this big mess to manually expand
+
+	//  configurations.data[0].features.subscription_update.products.length;
+
+	// We preserve the order stripe returns things in
+	const products = new Array(
+		configurations.data[0].features.subscription_update.products?.length
+	)
+	const pricePromises = (
+		configurations.data[0].features.subscription_update.products || []
+	)
+		.map((product, i) =>
+			product.prices.map(async (price, j) => {
+				const priceData = await stripeApiClient.prices.retrieve(price, {
+					expand: ["product"],
+				})
+				const cleanPriceData = {
+					...priceData,
+					product: (priceData.product as Stripe.Product).id,
+				}
+				if (!products[i]) {
+					products[i] = {
+						product: priceData.product,
+						prices: new Array(product.prices.length),
+					}
+					products[i].prices[j] = cleanPriceData
+				} else {
+					products[i].prices[j] = cleanPriceData
+				}
+			})
+		)
+		.flat()
+
+	let subscription: Stripe.Subscription | null = null
+	const subscriptionPromise = new Promise<void>(async (resolve) => {
+		const customer: any = await stripeApiClient.customers.retrieve(customerId, {
+			expand: ["subscriptions"],
+		})
+		subscription = customer.subscriptions.data[0] || null
+		if (subscription) {
+			subscription = await stripeApiClient.subscriptions.retrieve(
+				subscription.id,
+				{
+					expand: ["plan.product"],
+				}
+			)
+		}
+		resolve()
+	})
+
+	await Promise.all([...pricePromises, subscriptionPromise])
+
+	return { products, subscription }
 }
 
-async function redirectToCheckout({ customerId, body }:RedirectArgs) {
-  const configurations = await stripeApiClient.billingPortal.configurations.list(
-    {
-      is_default: true,
-      expand: ["data.features.subscription_update.products"],
-    }
-  );
+export interface RedirectArgs {
+	customerId: string
+	body: any
+}
+async function redirectToCustomerPortal({ customerId, body }: RedirectArgs) {
+	return await stripeApiClient.billingPortal.sessions.create({
+		customer: customerId,
+		return_url: body.returnUrl,
+	})
+}
 
-  // Make sure the price ID is in here somewhere
-  let go = false;
-  for (let product of configurations.data[0].features.subscription_update
-    .products || []) {
-    for (let price of product.prices) {
-      if (price === body.price) {
-        go = true;
-        break;
-      }
-    }
-  }
+async function redirectToCheckout({ customerId, body }: RedirectArgs) {
+	const configurations =
+		await stripeApiClient.billingPortal.configurations.list({
+			is_default: true,
+			expand: ["data.features.subscription_update.products"],
+		})
 
-  if (go) {
-    return await stripeApiClient.checkout.sessions.create({
-      customer: customerId,
-      success_url: body.successUrl,
-      cancel_url: body.cancelUrl,
-      line_items: [{ price: body.price, quantity: 1 }],
-      mode: "subscription",
-    });
-  }
-  return { error: "Error" };
+	// Make sure the price ID is in here somewhere
+	let go = false
+	for (let product of configurations.data[0].features.subscription_update
+		.products || []) {
+		for (let price of product.prices) {
+			if (price === body.price) {
+				go = true
+				break
+			}
+		}
+	}
+
+	if (go) {
+		return await stripeApiClient.checkout.sessions.create({
+			customer: customerId,
+			success_url: body.successUrl,
+			cancel_url: body.cancelUrl,
+			line_items: [{ price: body.price, quantity: 1 }],
+			mode: "subscription",
+			allow_promotion_codes: body.allowPromotionCodes,
+		})
+	}
+	return { error: "Error" }
 }
